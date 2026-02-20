75829790d6d2a7e083b4406ac579d03b1d25e65a
FOSSMan <137928236+0xfossman@users.noreply.github.com>
2026-02-20 15:10:48 +0100

    style: fix pint violations in controllers and middleware

---
+++ app/Http/Controllers/RosterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\StormforgeService;
+use Illuminate\Contracts\View\View;
+
+class RosterController extends Controller
+{
+    public function __construct(private readonly StormforgeService $stormforgeService) {}
+
+    public function index(): View
+    {
+        return view('roster.index', [
+            'members' => $this->stormforgeService->getGuildRoster(),
+        ]);
+    }
+}
